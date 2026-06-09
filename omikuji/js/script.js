a60eb039cd327d1408b1dcec6973b1f4f99f1b47
AZURE\user <azurecrimson756@gmail.com>
2026-06-09 16:17:26 +0900

    完成系

--- omikuji/js/script.js
+++ omikuji/js/script.js
@@ -1,7 +1,30 @@
+const resultText = document.getElementById('result');
+
 document.getElementById('draw').addEventListener('click', function () {
   const omikujiResults = ['大吉', '中吉', '小吉', '末吉', '凶', '大凶'];
   const result = omikujiResults[
     Math.floor(Math.random() * omikujiResults.length)];
-  document.getElementById('result').textContent =
-    `あなたの運勢は... ${result}!`;
+
+  resultText.textContent = `あなたの運勢は... ${result}!`;
+  resultText.className = `result ${getResultClass(result)}`;
 });
+
+function getResultClass(result) {
+  switch (result) {
+    case '大吉':
+      return 'daikichi';
+    case '中吉':
+      return 'chuukichi';
+    case '小吉':
+      return 'shoukichi';
+    case '末吉':
+      return 'suekichi';
+    case '凶':
+      return 'kyou';
+    case '大凶':
+      return 'daikyou';
+    default:
+      return '';
+  }
+}
+
